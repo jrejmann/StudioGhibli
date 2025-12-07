fffb97d23ad498f403ec6bab0c90d4cf7a04269f
Jakub Rejmann <jakubrejmann@gmail.com>
2025-12-07 13:23:17 +0100

    Initial Commit

---
+++ StudioGhibli/StudioGhibliApp.swift
@@ -0,0 +1,17 @@
+//
+//  StudioGhibliApp.swift
+//  StudioGhibli
+//
+//  Created by Kuba Rejmann on 07/12/2025.
+//
+
+import SwiftUI
+
+@main
+struct StudioGhibliApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ StudioGhibliTests/StudioGhibliTests.swift
@@ -0,0 +1,17 @@
+//
+//  StudioGhibliTests.swift
+//  StudioGhibliTests
+//
+//  Created by Kuba Rejmann on 07/12/2025.
+//
+
+import Testing
+@testable import StudioGhibli
+
+struct StudioGhibliTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
